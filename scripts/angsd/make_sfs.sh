86a369adc48c19fd619f3d4ebad56a8ee742ca2b
Elias Michel Oziolor <oziolor@gmail.com>
2019-01-19 17:41:00 -0800

    SAF finished for some samples and SFS was very quick; running thetas for 1,2,4,5,9,11; running VCFs on med

---
+++ scripts/angsd/make_sfs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -l
+
+#SBATCH -J sfs
+#SBATCH --array=9,11
+#SBATCH -e sfs%A-%a.o
+#SBATCH -o sfs%A-%a.o
+#SBATCH -t 06-00:00
+#SBATCH -n 8
+#SBATCH --mem=16G
+#SBATCH -p high
+#SBATCH --no-requeue
+
+pops=CAP_F\ CAP_H\ CAP_L\ CAP_S\ CAR_F\ CAR_H\ CAR_L\ CAR_S\ CUN_F\ CUN_L\ CUN_S
+
+one=$(echo $pops | cut -f $SLURM_ARRAY_TASK_ID -d ' ')
+
+
+#program and file
+my_sfs=/home/eoziolor/program/angsd/misc/realSFS
+in_saf=/home/eoziolor/guppy/data/angsd/$one\_small.saf.idx
+outdir=/home/eoziolor/guppy/data/angsd
+out_sfs=$one\.sfs
+
+#code
+
+$my_sfs $in_saf -maxIter 100 -P 8 -nSites 50000000 > $outdir/$out_sfs
---
+++ scripts/angsd/make_thetas.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -l
+
+#SBATCH -J thetas
+#SBATCH --array=1,2,4,5,9,11
+#SBATCH -e thetas%A-%a.o
+#SBATCH -o thetas%A-%a.o
+#SBATCH -t 06-00:00
+#SBATCH -n 8
+#SBATCH --mem=16G
+#SBATCH -p high
+#SBATCH --no-requeue
+
+pops=CAP_F\ CAP_H\ CAP_L\ CAP_S\ CAR_F\ CAR_H\ CAR_L\ CAR_S\ CUN_F\ CUN_L\ CUN_S
+
+one=$(echo $pops | cut -f $SLURM_ARRAY_TASK_ID -d ' ')
+
+#files
+list=/home/eoziolor/guppy/data/list/pops/$one\.txt
+genome=/home/eoziolor/guppy/data/genome/preticulata.fna
+keep=/home/eoziolor/guppy/data/angsd/keepsites.file
+outfile=/home/eoziolor/guppy/data/angsd/$one\_theta
+my_angsd=/home/eoziolor/program/angsd/angsd
+my_sfs=/home/eoziolor/guppy/data/angsd/$one\.sfs
+
+#Code
+$my_angsd \
+-bam $list \
+-out $outfile \
+-doThetas 1 \
+-fold 1 \
+-doSaf 1 \
+-pest  $my_sfs \
+-anc $genome \
+-sites $keep \
+-minMapQ 30 \
+-minQ 20 \
+-minind 10 \
+-GL 2
